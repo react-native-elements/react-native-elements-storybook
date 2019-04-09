da04f7c7c27d91bdcc1315641f42c9ef35a626fc
haruelrovix <surat@havit.web.id>
2019-04-09 17:49:52 +0700

    Initialise CRNWA

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import { Platform } from 'react-native';
+import React from 'react';
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import App from './App';
+
+describe('App', () => {
+  it('renders without errors', () => {
+    const tree = renderer.create(<App/>);
+    expect(tree.toJSON()).toBeTruthy();
+  });
+
+  it('example snapshot test on - ' + Platform.OS, () => {
+    const tree = renderer.create(<App/>);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
