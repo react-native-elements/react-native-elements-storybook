8453854aa2e79ecd7cc8530ffac3f60bf71ef5d5
haruelrovix <surat@havit.web.id>
2019-04-17 20:43:05 +0700

    Add story for Header

---
+++ stories/components/Header/MyCustomCenterComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MyCustomCenterComponent = () => (
+  <View>
+    <Text style={{ fontWeight: 'bold', color: '#fff' }}>
+      React Native Elements
+    </Text>
+  </View>
+)
+
+export default MyCustomCenterComponent;
---
+++ stories/components/Header/header.stories.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Text } from 'react-native';
+import { Header } from 'react-native-elements';
+
+import MyCustomLeftComponent from './MyCustomLeftComponent'
+import MyCustomCenterComponent from './MyCustomCenterComponent'
+import MyCustomRightComponent from './MyCustomRightComponent'
+
+storiesOf('Header|Header', module)
+
+  .add('with default components', () => (
+    <Header
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+      rightComponent={{ icon: 'home', color: '#fff' }}
+    />
+  ))
+
+  .add('Left aligned center component', () => (
+    <Header
+      placement="left"
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+      rightComponent={{ icon: 'home', color: '#fff' }}
+    />
+  ))
+
+  .add('with custom components passed in through props', () => (
+    <Header
+      leftComponent={<MyCustomLeftComponent />}
+      centerComponent={<MyCustomCenterComponent />}
+      rightComponent={<MyCustomRightComponent />}
+    />
+  ))
+
+  .add('with mixed components', () => {
+    const renderCenterComponent = () => (
+      <Text style={{ fontWeight: 'bold', color: '#fff' }}>
+        React Native Elements
+      </Text>
+    )
+
+    return (
+      <Header
+        leftComponent={<MyCustomLeftComponent />}
+        centerComponent={renderCenterComponent()}
+        rightComponent={{ icon: 'home', style: { color: '#fff' } }}
+      />
+    )
+  })
+
+  .add('with custom components passed in as children', () => (
+    <Header>
+      <MyCustomLeftComponent />
+      <MyCustomCenterComponent />
+      <MyCustomRightComponent />
+    </Header>
+  ))
+
+  .add('Header customisability', () => (
+    <Header
+      statusBarProps={{ barStyle: 'light-content' }}
+      barStyle="light-content" // or directly
+      leftComponent={<MyCustomLeftComponent />}
+      centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+      containerStyle={{
+        backgroundColor: '#3D6DCC',
+        justifyContent: 'space-around',
+      }}
+    />
+  ));
