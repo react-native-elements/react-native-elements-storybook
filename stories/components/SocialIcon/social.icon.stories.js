5579bf379a19f95e363bf006e9916a170f05d346
haruelrovix <surat@havit.web.id>
2019-04-17 22:17:54 +0700

    Add story for SocialIcon

---
+++ stories/components/SocialIcon/social.icon.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { SocialIcon } from 'react-native-elements';
+
+storiesOf('SocialIcon|Icon', module)
+
+  .add('basic usage', () => (
+    <SocialIcon type='twitter' />
+  ))
+
+  .add('with raised false', () => (
+    <SocialIcon
+      raised={false}
+      type='gitlab'
+    />
+  ))
+
+  .add('reverse icon color scheme', () => (
+    <SocialIcon
+      light
+      type='medium'
+    />
+  ))
+
+  .add('reverse and light', () => (
+    <SocialIcon
+      light
+      raised={false}
+      type='medium'
+    />
+  ));
