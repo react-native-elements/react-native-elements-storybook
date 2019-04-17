3eaaaa90bc523d88aa0511c080f4dfbe9ac10c94
haruelrovix <surat@havit.web.id>
2019-04-17 10:38:20 +0700

    Add story for ListItem

---
+++ stories/components/ListItem/listItem.badge.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { ListItem } from 'react-native-elements';
+
+storiesOf('ListItem|Badge', module)
+
+  .add('Example badge usage', () => {
+    return (
+      <ListItem
+        badge={{
+          value: 3,
+          textStyle: { color: 'orange' },
+          containerStyle: { marginTop: 20 }
+        }}
+        containerStyle={{
+          borderColor: 'rgb(32, 137, 220)',
+          borderWidth: 1
+        }}
+      />
+    )
+  });
---
+++ stories/components/ListItem/listItem.map.stories.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { View } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+storiesOf('ListItem|Using Map function', module)
+
+  .add('Implemented with avatar', () => {
+    const list = [
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      }
+    ]
+
+    return (
+      <View>
+        {
+          list.map((l, i) => (
+            <ListItem
+              key={i}
+              leftAvatar={{ source: { uri: l.avatar_url } }}
+              title={l.name}
+              subtitle={l.subtitle}
+            />
+          ))
+        }
+      </View>
+    )
+  })
+
+  .add('Implemented with link and icon', () => {
+    const list = [
+      {
+        title: 'Appointments',
+        icon: 'av-timer'
+      },
+      {
+        title: 'Trips',
+        icon: 'flight-takeoff'
+      }
+    ]
+
+    return (
+      <View>
+        {
+          list.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+              leftIcon={{ name: item.icon }}
+            />
+          ))
+        }
+      </View>
+    )
+  });
