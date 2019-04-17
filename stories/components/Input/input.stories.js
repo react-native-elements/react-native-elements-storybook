8c6d2c734645f08d38488b9dd5becba95b6d5eeb
haruelrovix <surat@havit.web.id>
2019-04-17 11:40:30 +0700

    Add story for Input

---
+++ stories/components/Input/input.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+
+storiesOf('Input|Input', module)
+
+  .add('Basic input', () => (
+    <Input
+      placeholder='BASIC INPUT'
+    />
+  ))
+
+  .add('Input with icon', () => (
+    <Input
+      placeholder='INPUT WITH ICON'
+      leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+    />
+  ))
+
+  .add('Input with custom icon', () => (
+    <Input
+      placeholder='INPUT WITH CUSTOM ICON'
+      leftIcon={
+        <Icon
+          name='user'
+          size={24}
+          color='black'
+        />
+      }
+    />
+  ))
+
+  .add('Input with error message', () => (
+    <Input
+      placeholder='INPUT WITH ERROR MESSAGE'
+      errorStyle={{ color: 'red' }}
+      errorMessage='ENTER A VALID ERROR HERE'
+    />
+  ));
