4ed1beef5cc0cf9490b52c1fc39ef431aa6c1df2
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-08 01:22:41 +0900

    chore: use full control mode

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,51 +6,54 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
-const path = require('path');
+const path = require("path");
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, '../node_modules/react-native/'),
-          path.resolve(__dirname, '../node_modules/react-native-elements/'),
-          path.resolve(__dirname, '../node_modules/react-native-ratings/'),
-          path.resolve(__dirname, '../node_modules/react-native-vector-icons/'),
-        ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: false,
-            presets: [
-              ['module:metro-react-native-babel-preset', { 'disableImportExportTransform': true }]
-            ],
-            plugins: [
-              // needed to support async/await
-              '@babel/plugin-transform-runtime',
-              '@babel/plugin-proposal-class-properties'
+module.exports = ({ config }) => {
+  config.module.rules.push(
+    // add your custom rules.
+    {
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, "../node_modules/react-native/"),
+        path.resolve(__dirname, "../node_modules/react-native-elements/"),
+        path.resolve(__dirname, "../node_modules/react-native-ratings/"),
+        path.resolve(__dirname, "../node_modules/react-native-vector-icons/")
+      ],
+      use: {
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: false,
+          presets: [
+            [
+              "module:metro-react-native-babel-preset",
+              { disableImportExportTransform: true }
             ]
-          }
+          ],
+          plugins: [
+            // needed to support async/await
+            "@babel/plugin-transform-runtime",
+            "@babel/plugin-proposal-class-properties"
+          ]
         }
-      },
-      // loader for png
-      {
-        test: /\.(gif|jpe?g|png|svg)$/,
-        use: {
-          loader: 'file-loader'
-        }
-      },
-      // loader for font icons
-      {
-        test: /\.ttf$/,
-        loader: 'url-loader',
-        include: path.resolve(__dirname, '../node_modules/react-native-vector-icons/'),
       }
-    ],
-  },
+    },
+    // loader for png
+    {
+      test: /\.(gif|jpe?g|png|svg)$/,
+      use: {
+        loader: "file-loader"
+      }
+    },
+    // loader for font icons
+    {
+      test: /\.ttf$/,
+      loader: "url-loader",
+      include: path.resolve(
+        __dirname,
+        "../node_modules/react-native-vector-icons/"
+      )
+    }
+  );
+
+  return config;
 };
