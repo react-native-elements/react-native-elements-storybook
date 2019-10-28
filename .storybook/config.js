42b41ab177babdbf036a2bed758792f659bba9db
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-28 18:41:40 +0900

    feat: add viewport addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+});
 
 configure(loadStories, module);
