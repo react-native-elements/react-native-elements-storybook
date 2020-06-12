abf06ffd626900649446dee94ec0a6fc5adccbf1
Alex Stokes <stokesbga@gmail.com>
2020-06-12 12:59:56 -0400

    update dependencies to latest

--- stories/config/storyLoader.js
+++ stories/config/storyLoader.js
@@ -1,63 +1,60 @@
-
 // Auto-generated file created by react-native-storybook-loader
 // Do not edit.
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../components/1stWelcome/welcome.stories');
-  require('../components/Avatar/avatar.icons.stories');
-  require('../components/Avatar/avatar.initials.stories');
-  require('../components/Avatar/avatar.placeholder.stories');
-  require('../components/Avatar/avatar.standard.stories');
-  require('../components/Badge/badge.stories');
-  require('../components/Button/button.stories');
-  require('../components/ButtonGroup/buttonGroup.stories');
-  require('../components/Card/card.stories');
-  require('../components/CheckBox/checkbox.stories');
-  require('../components/Divider/divider.stories');
-  require('../components/Header/header.stories');
-  require('../components/Icon/icon.stories');
-  require('../components/Image/image.stories');
-  require('../components/Input/input.stories');
-  require('../components/ListItem/listItem.badge.stories');
-  require('../components/ListItem/listItem.customView.stories');
-  require('../components/ListItem/listItem.flatList.stories');
-  require('../components/ListItem/listItem.map.stories');
-  require('../components/Pricing/pricing.stories');
-  require('../components/SocialIcon/social.button.stories');
-  require('../components/SocialIcon/social.icon.stories');
-  require('../components/Text/text.stories');
-  require('../components/Tile/tile.stories');
-  
+	require('../components/1stWelcome/welcome.stories');
+	require('../components/Avatar/avatar.icons.stories');
+	require('../components/Avatar/avatar.initials.stories');
+	require('../components/Avatar/avatar.placeholder.stories');
+	require('../components/Avatar/avatar.standard.stories');
+	require('../components/Badge/badge.stories');
+	require('../components/Button/button.stories');
+	require('../components/ButtonGroup/buttonGroup.stories');
+	require('../components/Card/card.stories');
+	require('../components/CheckBox/checkbox.stories');
+	require('../components/Divider/divider.stories');
+	require('../components/Header/header.stories');
+	require('../components/Icon/icon.stories');
+	require('../components/Image/image.stories');
+	require('../components/Input/input.stories');
+	require('../components/ListItem/listItem.badge.stories');
+	require('../components/ListItem/listItem.customView.stories');
+	require('../components/ListItem/listItem.flatList.stories');
+	require('../components/ListItem/listItem.map.stories');
+	require('../components/Pricing/pricing.stories');
+	require('../components/SocialIcon/social.button.stories');
+	require('../components/SocialIcon/social.icon.stories');
+	require('../components/Text/text.stories');
+	require('../components/Tile/tile.stories');
 }
 
 const stories = [
-  '../components/1stWelcome/welcome.stories',
-  '../components/Avatar/avatar.icons.stories',
-  '../components/Avatar/avatar.initials.stories',
-  '../components/Avatar/avatar.placeholder.stories',
-  '../components/Avatar/avatar.standard.stories',
-  '../components/Badge/badge.stories',
-  '../components/Button/button.stories',
-  '../components/ButtonGroup/buttonGroup.stories',
-  '../components/Card/card.stories',
-  '../components/CheckBox/checkbox.stories',
-  '../components/Divider/divider.stories',
-  '../components/Header/header.stories',
-  '../components/Icon/icon.stories',
-  '../components/Image/image.stories',
-  '../components/Input/input.stories',
-  '../components/ListItem/listItem.badge.stories',
-  '../components/ListItem/listItem.customView.stories',
-  '../components/ListItem/listItem.flatList.stories',
-  '../components/ListItem/listItem.map.stories',
-  '../components/Pricing/pricing.stories',
-  '../components/SocialIcon/social.button.stories',
-  '../components/SocialIcon/social.icon.stories',
-  '../components/Text/text.stories',
-  '../components/Tile/tile.stories',
-  
+	'../components/1stWelcome/welcome.stories',
+	'../components/Avatar/avatar.icons.stories',
+	'../components/Avatar/avatar.initials.stories',
+	'../components/Avatar/avatar.placeholder.stories',
+	'../components/Avatar/avatar.standard.stories',
+	'../components/Badge/badge.stories',
+	'../components/Button/button.stories',
+	'../components/ButtonGroup/buttonGroup.stories',
+	'../components/Card/card.stories',
+	'../components/CheckBox/checkbox.stories',
+	'../components/Divider/divider.stories',
+	'../components/Header/header.stories',
+	'../components/Icon/icon.stories',
+	'../components/Image/image.stories',
+	'../components/Input/input.stories',
+	'../components/ListItem/listItem.badge.stories',
+	'../components/ListItem/listItem.customView.stories',
+	'../components/ListItem/listItem.flatList.stories',
+	'../components/ListItem/listItem.map.stories',
+	'../components/Pricing/pricing.stories',
+	'../components/SocialIcon/social.button.stories',
+	'../components/SocialIcon/social.icon.stories',
+	'../components/Text/text.stories',
+	'../components/Tile/tile.stories'
 ];
 
 module.exports = {
